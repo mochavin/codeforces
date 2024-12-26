f638b1c794c9bc68378264abc9606487b3e9f2e6
Moch. Avin <5025221061@mhs.its.ac.id>
2024-12-27 06:05:10 +0700

    upsolved 2049

--- 2049/d.cpp
+++ 2049/d.cpp
@@ -11,17 +11,47 @@ ll N = 2e5 + 1;
 
 void solve() {
   ll n, m, k; cin >> n >> m >> k;
-  ll arr[n][m];
+  vector<vector<ll>> arr(n, vector<ll>(2 * m));
 
   for (int i = 0; i < n; i++) {
     for (int j = 0; j < m; j++) {
       cin >> arr[i][j];
+      arr[i][j + m] = arr[i][j];
     }
   }
 
   // the cost is kx + y, where x is operation row shift, and y is the sum of the visited
   // for minimum cost, use dp, the option is either right, down, shift.
   // how to count the shifting?
+
+  // there is f(x) and g(x)
+  // f(i, j) is the min cost to move to cell i,j
+  // g(i, j, x) is the min cost to move to i,j where row i is shifted left by x
+
+
+  // base row = 1, col = 0
+  vector<vector<ll>> dp(202, vector<ll>(202));
+  loop(i, n + 1) {
+    loop(j, m + 1) dp[i][j] = 1e18;
+  }
+
+  // basecase
+  dp[0][0] = 0;
+  loop(i, n) {
+    loop(j, m) {
+
+      // shift t times, penentuan ans untuk row i+1 and col j, j+1, ..., m
+      loop(t, m) {
+        ll cost = dp[i][j] + k * t;
+        for (int len = 1; j + len <= m; len++) {
+          cost += arr[i][j + t + len - 1];
+          // when to choose down
+          dp[i + 1][j + len - 1] = min(dp[i + 1][j + len - 1], cost);
+        }
+      }
+    }
+  }
+  cout << dp[n][m - 1] << endl;
 
 
   return;
