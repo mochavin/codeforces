32a7aec49cdf6f6da3ef3ce2bda8cf06cc5ecb0a
Moch. Avin <5025221061@mhs.its.ac.id>
2025-01-28 15:10:10 +0700

    upsolve 2044

--- 2044/g1.cpp
+++ 2044/g1.cpp
@@ -11,32 +11,29 @@ ll N = 2e5 + 1;
 
 void solve()
 {
-  int n; cin >> n;
-  vector<int> r(n + 1), d(n + 1);
+  ll n; cin >> n;
+  vector<ll> r(n + 1), foll(n + 1);
   loop1(i, n) {
     cin >> r[i];
-    d[r[i]]++;
+    foll[r[i]]++;
   }
-  set<pair<int, int>> s;
-  loop1(i, n) {
-    s.insert({ d[i], i });
-  }
-
-  int ans = 2;
-  queue<int> q;
-  while (s.size() && s.begin()->first == 0) {
-    while (s.size() && s.begin()->first == 0) {
-      int k = s.begin()->second; // index tidak ada yang nunjuk
-      auto it = s.find({ d[r[k]], r[k] });
-      d[r[k]]--;
-      if (it != s.end()) {
-        s.erase(it);
+  set<pair<ll, ll>> st;
+  loop1(i, n) st.insert({ foll[i], i });
+  ll ans = 2;
+  queue<ll> q;
+  while (!st.empty() and (*st.begin()).first == 0) {
+    while (!st.empty() and (*st.begin()).first == 0) {
+      ll k = (*st.begin()).second;
+      auto it = st.find({ foll[r[k]], r[k] });
+      foll[r[k]]--;
+      if (it != st.end()) {
+        st.erase(it);
         q.push(r[k]);
       }
-      s.erase(s.begin());
+      st.erase(st.begin());
     }
-    while (q.size()) {
-      s.insert({ d[q.front()], q.front() });
+    while (!q.empty()) {
+      st.insert({ foll[q.front()], q.front() });
       q.pop();
     }
     ans++;
